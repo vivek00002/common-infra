ffc6554b2ddd43a022cc09955f77fbcb28ca6bd6
EC2 Default User <ec2-user@ip-172-31-19-153.ec2.internal>
2025-03-03 15:13:05 +0000

    fnl

---
+++ index.ts
@@ -0,0 +1,21 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+
+// Configuration
+const domainName = "testcase1.duckdns.org";  // Replace with your actual domain
+
+// Create a Route 53 Hosted Zone
+const hostedZone = new aws.route53.Zone("hostedZone", {
+    name: domainName,
+});
+
+// Create an ACM Certificate
+const certificate = new aws.acm.Certificate("sslCertificate", {
+    domainName: domainName,
+    validationMethod: "DNS",
+}, { provider: new aws.Provider("useast1", { region: "us-east-1" }) });
+
+// Export values to be used by app-infra
+export const hostedZoneId = hostedZone.id;
+export const certificateArn = certificate.arn;
+export const domain = domainName;
